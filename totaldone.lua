f8bc0fc6d19eace4b9abdaaeec7dd4417831099d
MORTEX <moegma8@gmail.com>
2025-03-24 14:33:55 +0100

    Update totaldone.lua

--- totaldone.lua
+++ totaldone.lua
@@ -30,7 +30,7 @@ task.spawn(function()
                 cashLabel.Text = tostring(cashValue.Value)
             end
         end
-        task.wait(5) -- Update every second
+        task.wait(5) 
     end
 end)
 
